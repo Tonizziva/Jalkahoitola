6a17c5911cd8742bf65c4a818f80e95e4f4ffa57
Tonizziva <36418433+Tonizziva@users.noreply.github.com>
2018-09-15 18:52:19 +0300

    osa3

---
+++ Jalkahoitola/Controllers/AsiakasController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Jalkahoitola.Models;
+
+namespace Jalkahoitola.Controllers
+{
+    public class AsiakasController : Controller
+    {
+        private Database1Entities db = new Database1Entities();
+
+        // GET: Asiakas
+        public ActionResult Index()
+        {
+            return View(db.Asiakas.ToList());
+        }
+
+        // GET: Asiakas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Asiakas asiakas = db.Asiakas.Find(id);
+            if (asiakas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(asiakas);
+        }
+
+        // GET: Asiakas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Asiakas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AsiakasID,Sukunimi,Etunimi,Postiosoite,Postitmp,Puhnro,Sposti")] Asiakas asiakas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Asiakas.Add(asiakas);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(asiakas);
+        }
+
+        // GET: Asiakas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Asiakas asiakas = db.Asiakas.Find(id);
+            if (asiakas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(asiakas);
+        }
+
+        // POST: Asiakas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AsiakasID,Sukunimi,Etunimi,Postiosoite,Postitmp,Puhnro,Sposti")] Asiakas asiakas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(asiakas).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(asiakas);
+        }
+
+        // GET: Asiakas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Asiakas asiakas = db.Asiakas.Find(id);
+            if (asiakas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(asiakas);
+        }
+
+        // POST: Asiakas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Asiakas asiakas = db.Asiakas.Find(id);
+            db.Asiakas.Remove(asiakas);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
